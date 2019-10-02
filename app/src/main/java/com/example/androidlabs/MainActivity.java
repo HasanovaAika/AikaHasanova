1d24390583c74c0b01bcc7f7c5d4eba66564ca45
HasanovaAika <55498592+HasanovaAika@users.noreply.github.com>
2019-10-02 11:51:31 -0400

    finished lab 3

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences preferences;
+    EditText editEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lab3);
+
+        editEmail = findViewById(R.id.userEmail);
+        preferences = getSharedPreferences("prefsFileName", Context.MODE_PRIVATE);
+        String saveEmail = preferences.getString("ReserveName", "");
+        editEmail.setText(saveEmail);
+
+        Button loginBtn = findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener(v -> {
+
+            Intent profile = new Intent(MainActivity.this, ProfileActivity.class);
+
+            profile.putExtra("EmailTyped", editEmail.getText().toString());
+            startActivity(profile);
+
+
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor edit = preferences.edit();
+        edit.putString("ReserveName", editEmail.getText().toString());
+        edit.apply();
+
+
     }
 }
---
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -0,0 +1,96 @@
+package com.example.androidlabs;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class ProfileActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageButton mImageButton;
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e(ACTIVITY_NAME, "In function:" + "onActivityResult()");
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+            dispatchTakePictureIntent();
+
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Log.e(ACTIVITY_NAME, "In function:" + "onCreate()");
+
+        Intent loginPage =getIntent();
+        String email = loginPage.getStringExtra("EmailTyped");
+        EditText emailSaved =(EditText)findViewById(R.id.emailProfileText2);
+        emailSaved.setText(email);
+
+        mImageButton = (ImageButton) findViewById(R.id.pictureButton);
+
+        mImageButton.setOnClickListener(c -> {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
+    });
+    }
+
+
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.e(ACTIVITY_NAME, "In function:" + "onStart()");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.e(ACTIVITY_NAME, "In function:" + "onResume()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.e(ACTIVITY_NAME, "In function:" + "onPause()");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.e(ACTIVITY_NAME, "In function:" + "onStop()");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME, "In function:" + "onDestroy()");
+    }
+
+}
